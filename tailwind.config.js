4ae3e56b0fbe0395e311052981340e43d3a78e7d
Oskar <o.pulczynski@gmail.com>
2024-10-22 18:06:01 +0200

    Added styling for most sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html'],
+  content: ["./src/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
-      }
+        inter: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        "3xl": "0 0 10px 0 rgba(255, 255, 255, 0.08)",
+      },
     },
   },
   plugins: [],
-}
-
+};
